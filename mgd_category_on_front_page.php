b2e33c6baef3f0437dc4b8d4b4f500bba19eb26c
Mike Dean <contact@michaeldean.ca>
2020-10-01 01:15:57 +0000

    initial plugin commit

---
+++ mgd_category_on_front_page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Category on Front Page
+ * Plugin URI:      https://github.com/mykedean/mgd_category_on_front_page
+ * Description:     mgd_category_on_front_page
+ * Author:          Michael Gary Dean
+ * Author URI:      michaeldean.ca
+ * Text Domain:     mgd_category_on_front_page
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Mgd_category_on_front_page
+ */
